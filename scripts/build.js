671ad623a2fcf268e45340316084b80690e7bdd1
JNieto <jorge.bbz@gmail.com>
2022-09-14 16:44:45 +0100

    chore: project config stuff

--- scripts/build.js
+++ scripts/build.js
@@ -1,34 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const Path = require('path');
 const Chalk = require('chalk');
 const FileSystem = require('fs');
 const compileTs = require('./private/tsc');
 
 function buildRenderer() {
-    const Vite = require('vite');
-    const viteConfig = require(Path.join(__dirname, '..', 'config', 'vite.js'));
+  const Vite = require('vite');
+  const viteConfig = require(Path.join(__dirname, '..', 'config', 'vite.js'));
 
-    return Vite.build({
-        ...viteConfig,
-        base: './',
-        mode: 'production'
-    });
+  return Vite.build({
+    ...viteConfig,
+    base: './',
+    mode: 'production',
+  });
 }
 
 function buildMain() {
-    const mainPath = Path.join(__dirname, '..', 'src', 'main');
-    return compileTs(mainPath);
+  const mainPath = Path.join(__dirname, '..', 'src', 'main');
+  return compileTs(mainPath);
 }
 
 FileSystem.rmSync(Path.join(__dirname, '..', 'build'), {
-    recursive: true,
-    force: true,
-})
+  recursive: true,
+  force: true,
+});
 
 console.log(Chalk.blueBright('Transpiling renderer & main...'));
 
 Promise.allSettled([
-    buildRenderer(),
-    buildMain(),
+  buildRenderer(),
+  buildMain(),
 ]).then(() => {
-    console.log(Chalk.greenBright('Renderer & main successfully transpiled! (ready to be built with electron-builder)'));
+  console.log(Chalk.greenBright('Renderer & main successfully transpiled! (ready to be built with electron-builder)'));
 });
